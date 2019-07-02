de16ae71238ee7de51dbcd31b9d7925dd0435ae5
Carlos Roig <carroisal3@gmail.com>
2019-07-02 22:56:42 +0200

    First Commit

---
+++ SavingFlippingVideos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SavingFlippingVideos
+//
+//  Created by Carlos Roig on 02/07/2019.
+//  Copyright © 2019 Vitcord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
